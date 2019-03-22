931062f97908fac34bdda30082ed6484063887e7
Scott Williams <scottcolemanwilliams@gmail.com>
2019-03-22 18:22:27 -0500

    added FeatureTypes component

--- src/Features.js
+++ src/Features.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FeatureTypes from './FeatureTypes';
 
 export default function Features(props) {
   
@@ -6,28 +7,38 @@ export default function Features(props) {
   return(
 
     Object.keys(props.features)
-    .map(key => {
-      const options = props.features[key].map((item, index) => {
-        const selectedClass = item.name === props.selected[key].name ? 'feature__selected' : '';
-        const featureClass = 'feature__option ' + selectedClass;
-        return <li key={index} className="feature__item">
-          <div className={featureClass}
-            
-            onClick={e => props.updateFeature(key, item)}>
-              { item.name }
-              ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                .format(item.cost) })
-          </div>
-        </li>
-      });
+    .map(key => 
 
-      return <div className="feature" key={key}>
-        <div className="feature__name">{key}</div>
-        <ul className="feature__list">
-          { options }
-        </ul>
-      </div>
-    })     
+    <FeatureTypes 
+      key = {key}
+      features = {props.features}
+      id = {key}
+      updateFeature = {props.updateFeature}
+      selected = {props.selected}
+    />
+      
+    )     
 
   )
 }
+
+// const options = props.features[key].map((item, index) => {
+//   const selectedClass = item.name === props.selected[key].name ? 'feature__selected' : '';
+//   const featureClass = 'feature__option ' + selectedClass;
+//   return <li key={index} className="feature__item">
+//     <div className={featureClass}
+      
+//       onClick={e => props.updateFeature(key, item)}>
+//         { item.name }
+//         ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+//           .format(item.cost) })
+//     </div>
+//   </li>
+// });
+
+// return <div className="feature" key={key}>
+//   <div className="feature__name">{key}</div>
+//   <ul className="feature__list">
+//     { options }
+//   </ul>
+// </div>
